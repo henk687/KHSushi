af2000441b128bee88c0300c17c1717123d483b1
henk687 <henkleenheer@upcmail.nl>
2023-07-11 19:31:11 +0200

    changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-import css from '../styles/Hero.module.css'
-import Sushi from '../assets/sushi1.png'
-import HeroImage from '../assets/HeroImage.png'
-import Sushi4 from '../assets/sushi-box-4.jpg'
-import Link from 'next/link'
+import Image from "next/image";
+import css from "../styles/Hero.module.css";
+import Sushi from "../assets/sushi1.png";
+import HeroImage from "../assets/HeroImage.png";
+import Sushi4 from "../assets/sushi-box-4.jpg";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -13,47 +13,46 @@ export default function Hero() {
         <div className={css.left}>
           <div className={css.sushiDiv}>
             <span>Snelle bezorging</span>
-            <Image src={Sushi} alt="" width={40} height={25}/>
+            <Image src={Sushi} alt="" width={40} height={25} />
           </div>
-        
+
           <div className={css.heroText}>
             <span>Wij bezorgen</span>
             <span>De lekkerste</span>
             <span>
-
-              Verse <span style={{color: "var(--themeRed)"}}>Sushi</span>
+              Verse <span style={{ color: "var(--themeRed)" }}>Sushi</span>
             </span>
           </div>
 
           <span className={css.miniText}>
-            Wij bezorgen Sushi in de omgeving Almelo vanaf 1 Mei 2023
+            Wij bezorgen Sushi in de omgeving Almelo vanaf 1 Januari 2024
           </span>
         </div>
         {/* rigth side */}
         <div className={css.right}>
           <div className={css.imageContainer}>
-            <Image src={HeroImage} alt="" layout='intrinsic'/>
+            <Image src={HeroImage} alt="" layout="intrinsic" />
           </div>
 
           <Link href={`./sushi/sushi-box-1`}>
             <div className={css.detailsTop}>
               <span>Sushi box 1</span>
               <span>
-                <span style={{color: "var(--themeRed)"}}>€</span> 20
+                <span style={{ color: "var(--themeRed)" }}>€</span> 20
               </span>
             </div>
           </Link>
 
           <div className={css.Sushi}>
             <div>
-              <Image src={Sushi4} alt="" objectFit='cover'/>
+              <Image src={Sushi4} alt="" objectFit="cover" />
             </div>
 
             <Link href={`./sushi/sushi-box-4`}>
               <div className={css.details}>
                 <span>Sushi box 4</span>
                 <span>
-                  <span style={{color: "var(--themeRed)"}}>€</span> 21
+                  <span style={{ color: "var(--themeRed)" }}>€</span> 21
                 </span>
               </div>
             </Link>
@@ -61,5 +60,5 @@ export default function Hero() {
         </div>
       </div>
     </div>
-  )
+  );
 }
